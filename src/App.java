caf894abd222a2a56cd3e8a472c76c55e4c959ed
Makrand <makrandshingare@gmail.com>
2021-11-23 09:28:46 +0530

    added sample line

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("hello world Wecome");
     }
+System.out.println("Sample");
 }
